a11adaf34967a3f1a6cae1123dcfd1fcaaebc58b
Ellen Jung <90542859+ellenjung24@users.noreply.github.com>
2024-09-23 13:22:26 -0700

    added media query for the navbar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -2,9 +2,11 @@ import React, { useEffect, useState } from 'react';
 import './Navbar.scss';
 import { Link } from 'react-scroll';
 import logo from '../../assets/logo.png';
+import menu_icon from '../../assets/menu-icon.png';
 
 const Navbar = () => {
   const [sticky, setSticky] = useState(false);
+  const [mobileMenu, setMobileMenu] = useState(false);
 
   useEffect(() => {
     const handleScroll = () => {
@@ -18,16 +20,21 @@ const Navbar = () => {
     };
   }, []);
 
+  const toggleMenu = () => {
+    mobileMenu ? setMobileMenu(false) : setMobileMenu(true);
+  }
+
   return (
     <nav className={`container ${sticky ? 'dark-nav' : ''}`}>
-      <img src={logo} alt="" className='logo' style={{ width: '150px', marginLeft: '100px'}} />
-      <ul>
+      <img src={logo} alt="" className='logo' />
+      <ul className={mobileMenu ? '' : 'hide-mobile-menu'}>
         <li><Link to="hero" smooth={true} offset={0} duration={500}>Home</Link></li>
         <li><Link to="about" smooth={true} offset={-150} duration={500}>About us</Link></li>
         <li><Link to="services" smooth={true} offset={-260} duration={500}>Services</Link></li>
         <li><Link to="contact" smooth={true} offset={-260} duration={500}>Contact us</Link></li>
-        <li><a href="https://app.acuityscheduling.com/schedule.php?owner=32025918" target="_blank" className='btn'>Book online</a></li>
+        <li><a href="https://app.acuityscheduling.com/schedule.php?owner=32025918" target="_blank" className='btn'>Book</a></li>
       </ul>
+      <img src={menu_icon} alt="" className='menu-icon' onClick={toggleMenu}/>
     </nav>
   );
 };
