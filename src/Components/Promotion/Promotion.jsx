2cb60ae8cc661e92c0a4df7e4af0ce8abc43a7a5
Ellen Jung <90542859+ellenjung24@users.noreply.github.com>
2024-09-30 23:28:48 -0700

    worked on the promotion section

--- src/Components/Promotion/Promotion.jsx
+++ src/Components/Promotion/Promotion.jsx
@@ -8,7 +8,7 @@ const Promotion = () => {
         <div className="promotion">
             <h3>Grand Opening Event</h3>
             <img src={freeNailClipping} alt="" />
-            <p>EXPIRES October 31, 2024</p>
+            <p>EXPIRES November 30, 2024</p>
             
         </div>
   )
