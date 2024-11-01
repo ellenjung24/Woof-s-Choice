d33c0d4e83bcc1ab7374f1cd3e3a55301afe9cd8
Ellen Jung <90542859+ellenjung24@users.noreply.github.com>
2024-10-31 19:46:36 -0700

    modified the promotion part

--- src/Components/Promotion/Promotion.jsx
+++ src/Components/Promotion/Promotion.jsx
@@ -8,7 +8,8 @@ const Promotion = () => {
         <div className="promotion">
             <h3>Grand Opening Event</h3>
             <img src={freeNailClipping} alt="" />
-            <p>EXPIRES November 30, 2024</p>
+            <p className='expiration'>EXPIRES December 15, 2024</p>
+            <p className='explanation'>* Please call to book an appointment for nail clipping! A free nail clipping is available once per customer. *</p>
             
         </div>
   )
