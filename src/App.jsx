2fac4997103dae8a5cf6d8208458b213f5a75e8f
Ellen Jung <90542859+ellenjung24@users.noreply.github.com>
2024-12-28 06:54:56 -0800

    removed promotion

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ const App = () => {
     <div>
       <Navbar />
       <Hero />
-      <Promotion />
+      {/* <Promotion /> */}
       <div className="container">
         {/* <Title subTitle="About us" context="When it comes to pet grooming we believe that:" /> */}
         <About />
