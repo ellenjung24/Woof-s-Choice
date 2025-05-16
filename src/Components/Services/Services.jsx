40c7f0ef798d5de1c15c84ef82412b54d0b28157
Ellen Jung <90542859+ellenjung24@users.noreply.github.com>
2025-05-15 22:51:00 -0700

    changed prices

--- src/Components/Services/Services.jsx
+++ src/Components/Services/Services.jsx
@@ -57,16 +57,20 @@ const Services = () => {
       </div>
 
       {/* <div className="warning">Prices depend on * size of dog, breed of dog, condition of the coat, type of coat and behaviour of dog *</div> */}
-      <div className="warning">* Prices may adjust according to their specific needs, breed, size and coat type & condition. *</div>
+      <div className="warning">
+        * Prices may adjust according to their specific needs, breed, size and
+        coat type & condition. *
+      </div>
       <div className="prices">
         <div className="all-services">
           <div className="price">
             <h2>FULL GROOMING</h2>
             <p className="description">
               <i>
-                Includes full clipping, hand scissoring to finish, bath with premium shampoo & conditioner and
-                blow dry, brush out, pads and paws & nail trim, ear
-                cleaning, sanitary trim, anal glands. Any extra de-matting is additional charge ($20).
+                Includes full clipping, hand scissoring to finish, bath with
+                premium shampoo & conditioner and blow dry, brush out, pads and
+                paws & nail trim, ear cleaning, sanitary trim, anal glands. Any
+                extra de-matting is additional charge ($15).
               </i>
             </p>
             <ul>
@@ -83,9 +87,10 @@ const Services = () => {
             <h2>BATH AND TIDY</h2>
             <p className="description">
               <i>
-              Includes bath with premium shampoo & conditioner and
-                blow dry, brush out, pads and paws & nail trim, ear
-                cleaning, sanitary trim, anal glands. Any extra de-matting is additional charge ($20).
+                Includes bath with premium shampoo & conditioner and blow dry,
+                brush out, pads and paws & nail trim, ear cleaning, sanitary
+                trim, anal glands. Any extra de-matting is additional charge
+                ($15).
               </i>
             </p>
             <ul>
@@ -110,7 +115,13 @@ const Services = () => {
               <li>Ear cleaning : $15</li>
               <li>Teeth brushing : $10</li>
               <li>Face tidy : $20</li>
-              <li>De-shedding : $15</li>
+            </ul>
+          </div>
+          <div className="price">
+            <h2>Extra/Add-ons</h2>
+            <ul style={{ "margin-top": "20px" }}>
+              <li>De-matting : $15/15 mins</li>
+              <li>De-shedding : $15/15mins</li>
               <li>Poodle feet : $20</li>
               <li>Behaviour : + $20-30</li>
             </ul>
@@ -128,8 +139,9 @@ const Services = () => {
             <p className="policy-title">Cancellation/Rescheduling Policy:</p>
             <p className="policy-description">
               Cancellation notice of 24 hours is required. Notice of less than
-              24 hours is subjected to a $15 cancellation fee. You may choose to reschedule
-              your appointment with at least 24 hours notice at no charge.
+              24 hours is subjected to a $15 cancellation fee. You may choose to
+              reschedule your appointment with at least 24 hours notice at no
+              charge.
             </p>
           </div>
           <div>
@@ -137,9 +149,9 @@ const Services = () => {
             <p className="policy-description">
               We appreciate a call if you are anticipating to be late. If you
               are more than 10 minutes late without notification, a fee may be
-              applied at our discretion. We may also choose to reschedule you due
-              to time restraints. Late Pick ups: please pick up your pets at the
-              notified time. 
+              applied at our discretion. We may also choose to reschedule you
+              due to time restraints. Late Pick ups: please pick up your pets at
+              the notified time.
               {/* Dogs not picked up within a reasonable time will be
               subjected to a $1/minute fee or at end of day, a $50/night
               boarding fee after 30 minutes of no notification. */}
@@ -153,40 +165,53 @@ const Services = () => {
               There will also be a $20 charge towards your next appointment.
             </p>
           </div>
-          <h2>Extra/Add-ons</h2>
-          <ul style={{ "margin-top": "20px" }}>
-              <li>De-matting : $20/15 mins</li>
-              <li>De-shedding : $15/15mins</li>
-              <li>Poodle feet : $20</li>
-              <li>Behaviour : + $20-30</li>
-          </ul>
         </div>
       </div>
 
-{/* ADDED - by HDC */}
-    <div className="service_grid">
-      <div className="service_grid-left">
-        <div className="service_text">
-          <h3 className="discount">15&#37; Off</h3>
-          <h4>&nbsp;On Full Grooming &amp; Bath &amp; Tidy&nbsp;</h4>
+      {/* ADDED - by HDC */}
+      <div className="service_grid">
+        <div className="service_grid-left">
+          <div className="service_text">
+            <h3 className="discount">15&#37; Off</h3>
+            <h4>&nbsp;On Full Grooming &amp; Bath &amp; Tidy&nbsp;</h4>
+          </div>
+        </div>
+        <div className="service_grid-right">
+          <h2>Thank You Promotion</h2>
+          <h3>through May &amp; June</h3>
+          <p>Expires on June 30, 2025</p>
+          <div className="padding-30"></div>
+          <a
+            href="https://app.acuityscheduling.com/schedule.php?owner=33051599"
+            target="_blank"
+            className="btn"
+          >
+            BOOK NOW
+          </a>
+          <div className="padding-20"></div>
+          <div className="disclaimer">
+            *1 time only. <br />
+            **This offer cannot be combined with any other offer. No cash value.
+            Limit one service per visit.
+          </div>
         </div>
       </div>
-      <div className="service_grid-right">
-        <h2>Thank You Promotion</h2>
-        <h3>through May &amp; June</h3>
-        <p>Expires on June 30, 2025</p>
-        <div className="padding-30"></div>
-        <a href="https://app.acuityscheduling.com/schedule.php?owner=33051599" target="_blank" className='btn'>BOOK NOW</a>
-        <div className="padding-20"></div>
-        <div className="disclaimer">*1 time only. <br />**This offer cannot be combined with any other offer. No cash value. Limit one service per visit.</div>
-      </div>
-    </div>
-{/* /ADDED - by HDC */}
+      {/* /ADDED - by HDC */}
 
       <div className="booking">
         <h1>Book Online below or call {isMobile && <br />} 604-731-1692</h1>
         <div className="book-online">
-          <iframe src="https://app.acuityscheduling.com/schedule.php?owner=33051599&ref=embedded_csp" className="booking-system" title="Schedule Appointment" height="800" frameBorder="0"></iframe><script src="https://embed.acuityscheduling.com/js/embed.js" type="text/javascript"></script>
+          <iframe
+            src="https://app.acuityscheduling.com/schedule.php?owner=33051599&ref=embedded_csp"
+            className="booking-system"
+            title="Schedule Appointment"
+            height="800"
+            frameBorder="0"
+          ></iframe>
+          <script
+            src="https://embed.acuityscheduling.com/js/embed.js"
+            type="text/javascript"
+          ></script>
         </div>
       </div>
     </>
